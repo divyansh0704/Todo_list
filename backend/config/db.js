19a74417395516f4e1bdae8513bde6f7d3c39cec
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-07-03 12:39:32 +0530

    updated backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,15 @@
 const {Sequelize}= require("sequelize");
-require("dotenv").config({path:"../.env"})
+const path = require("path")
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
 
-const sequelize = new Sequelize("DB_NAME","DB_USER","DB_PASS",{
-    host: 'DB_HOST',
-    dialect: 'DB_DIALECT',
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+    host:process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect:process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: false                   //  Freesqldatabase does not need SSL
+    },
+    logging: false 
 });
 
 module.exports = sequelize;
